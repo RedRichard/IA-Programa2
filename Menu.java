29c61eb574e01805b78f8c9797e66f0217df048e
ivarog <42422542+ivarog@users.noreply.github.com>
2018-08-16 13:51:13 -0500

    Add files via upload

--- Menu.java
+++ Menu.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 
 //Clase encargada de desplegar el menu
 public class Menu{
-    private Polinomio poli;
+    private Polinomio poli,poli2;
     private float escalar;
     public Menu()
     {
@@ -15,6 +15,12 @@ public class Menu{
                 SubmenuEscalar("Introduzca el punto: ");
                 MostrarMensaje("El valor en el punto es: " + String.valueOf(Polinomio.valorPunto(poli, escalar)));
                 break;
+            case "2":
+                Submenu2Polinomios();
+                MostrarMensaje("El valor de la suma es: " + Polinomio.suma(poli, poli2));
+            case "3":
+                Submenu2Polinomios();
+                MostrarMensaje("El valor de la resta es: " + Polinomio.resta(poli, poli2));
             default:
                 //En caso de que la opcion no sea válida se ejecuta el menu otravez
                 Menu menu = new Menu();
@@ -24,6 +30,11 @@ public class Menu{
     public void SubmenuPolinomio(){
         poli = new Polinomio(JOptionPane.showInputDialog(null,"Escriba los coeficientes del polinomio separados por comas\nEjem.  10,2,7,40,56" ).split(","));
     }
+    
+    public void Submenu2Polinomios(){
+        poli = new Polinomio(JOptionPane.showInputDialog(null,"Escriba los coeficientes del polinomio separados por comas\nEjem.  10,2,7,40,56" ).split(","));
+        poli2 = new Polinomio(JOptionPane.showInputDialog(null,"Escriba los coeficientes del polinomio separados por comas\nEjem.  10,2,7,40,56" ).split(","));
+    }
 
     public void SubmenuEscalar(String mensaje){
         escalar = Float.parseFloat(JOptionPane.showInputDialog(null, mensaje));
@@ -31,6 +42,7 @@ public class Menu{
 
     public void MostrarMensaje(String mensaje){
         JOptionPane.showMessageDialog(null, mensaje);
+        Menu menu = new Menu();
     }
 
     public String MostrarMensajeInput(String mensaje){
--- Polinomio.java
+++ Polinomio.java
@@ -1,4 +1,5 @@
 import java.lang.Math; 
+import java.util.Arrays;
 
 public class Polinomio{
     private String [] coeficientes;
@@ -26,7 +27,63 @@ public class Polinomio{
         return auxResultado;
     }
 
-    public static void suma(Polinomio poli1, Polinomio poli2){
- 
+    public static String suma(Polinomio poli1, Polinomio poli2){
+        //Obtenemos los coeficientes de polinomio
+        String [] coeficientes1=poli1.getCoeficientes(),coeficientes2=poli2.getCoeficientes();
+        String [] suma=new String[poli1.grado+1];
+        //Se recorre el arreglo y se suman
+        for(int i=0;i<=poli1.getGrado();i++)
+        {
+            suma[i]=Float.toString(Float.parseFloat(coeficientes2[i])+Float.parseFloat(coeficientes1[i]));
+        }
+        //Realiza una cadena con el resultado
+        int grado=poli1.grado;
+        String resultado="";
+        for(int i=0;i<suma.length-1;i++)
+        {
+            if(Float.parseFloat(suma[i+1])>0)
+            {
+                resultado=resultado+suma[i]+"x^"+grado+" +";
+                grado=grado-1;
+            }
+            else
+            {
+                resultado=resultado+suma[i]+"x^"+grado+" ";
+                grado=grado-1;
+            }
+        }
+        resultado=resultado+suma[suma.length-1]+" x^"+grado;
+        System.out.println(resultado);
+        return resultado;
+    }
+    
+        public static String resta(Polinomio poli1, Polinomio poli2){
+        //Obtenemos los coeficientes de polinomio
+        String [] coeficientes1=poli1.getCoeficientes(),coeficientes2=poli2.getCoeficientes();
+        String [] suma=new String[poli1.grado+1];
+        //Se recorre el arreglo y se restan
+        for(int i=0;i<=poli1.getGrado();i++)
+        {
+            suma[i]=Float.toString(Float.parseFloat(coeficientes1[i])-Float.parseFloat(coeficientes2[i]));
+        }
+        //Realiza una cadena con el resultado
+        int grado=poli1.grado;
+        String resultado="";
+        for(int i=0;i<suma.length-1;i++)
+        {
+            if(Float.parseFloat(suma[i+1])>0)
+            {
+                resultado=resultado+suma[i]+"x^"+grado+" +";
+                grado=grado-1;
+            }
+            else
+            {
+                resultado=resultado+suma[i]+"x^"+grado+" ";
+                grado=grado-1;
+            }
+        }
+        resultado=resultado+suma[suma.length-1]+" x^"+grado;
+        System.out.println(resultado);
+        return resultado;
     }
 }
